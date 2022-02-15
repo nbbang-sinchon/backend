935e0473839d88fb6ac0a40eaf06f161ffa65db4
kenokim <keno1413@gmail.com>
2022-02-15 13:21:34 +0900

    Feat : 테스트 환경 구성
    JUnit 테스트를 위한 환경 설정했습니다.

--- src/test/java/nbbang/com/nbbang/domain/party/repository/ManyPartyRepositoryTest.java
+++ src/test/java/nbbang/com/nbbang/domain/party/repository/ManyPartyRepositoryTest.java
@@ -1,6 +1,9 @@
 package nbbang.com.nbbang.domain.party.repository;
 
+import nbbang.com.nbbang.domain.party.domain.Party;
+import nbbang.com.nbbang.domain.party.service.PartyService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -9,6 +12,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @ActiveProfiles("test")
 class ManyPartyRepositoryTest {
+    @Autowired PartyRepository partyRepository;
+    @Autowired PartyService partyService;
+
     @Test
     public void filterTest() {
 
