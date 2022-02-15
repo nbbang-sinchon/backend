88572a7966b42c7a3ac25b54d0dcf6f7b9a3e8b3
kenokim <keno1413@gmail.com>
2022-02-15 12:32:01 +0900

    Feat : 쿼리 dsl config 파일 추가
    global 패키지의 config 패키지에 쿼리 dsl config 파일 추가하였습니다.

--- src/main/java/nbbang/com/nbbang/domain/party/repository/ManyPartyRepositorySupportImpl.java
+++ src/main/java/nbbang/com/nbbang/domain/party/repository/ManyPartyRepositorySupportImpl.java
@@ -1,13 +1,16 @@
 package nbbang.com.nbbang.domain.party.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import nbbang.com.nbbang.domain.party.domain.Party;
-import nbbang.com.nbbang.domain.party.dto.PartyFindRequestDto;
 import nbbang.com.nbbang.domain.party.dto.PartyFindRequestFilterDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+@RequiredArgsConstructor
 public class ManyPartyRepositorySupportImpl implements ManyPartyRepositorySupport {
 
+    private final JPAQueryFactory query;
 
     @Override
     public Page<Party> findAllByRequestDto(Pageable pageable, PartyFindRequestFilterDto requestFilterDto) {
