cefef8604bc2967abf1bfe50eea737f66124ce08
kenokim <keno1413@gmail.com>
2022-02-16 00:18:21 +0900

    Feat : 파티 리스트 조회 dto 필드 추가
    위치로 검색하는 필드를 추가하였습니다.

--- src/main/java/nbbang/com/nbbang/domain/party/dto/PartyFindRequestDto.java
+++ src/main/java/nbbang/com/nbbang/domain/party/dto/PartyFindRequestDto.java
@@ -1,18 +1,36 @@
 package nbbang.com.nbbang.domain.party.dto;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nbbang.com.nbbang.domain.member.dto.Place;
 import nbbang.com.nbbang.global.dto.PageableDto;
+import nbbang.com.nbbang.global.support.validation.ValueOfEnum;
 import org.springframework.data.domain.PageRequest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PartyFindRequestDto extends PageableDto {
+
     private List<String> places;
+    @Parameter(description = "true -> ON 인 파티만, false(null) -> 모든 파티")
     private Boolean showOngoing;
+    @Parameter(description = "제목 검색어")
     private String search;
+
+    @Parameter(hidden = true)
+    public List<Place> getPlaces() {
+        List<Place> res = new ArrayList<>();
+        places.stream().forEach(p -> res.add(Place.valueOf(p)));
+        return res;
+    }
+
+    public List<String> getPlacesString() {
+        return places;
+    }
 }
