88997e469da41f08a6b6964654885406643eb8dd
hyunkyungju <wngusrudrid@naver.com>
2022-02-20 15:45:08 +0900

    Refactor : 쿼리 타입 static import
    쿼리 타입을 static import 하였습니다.

--- src/main/java/nbbang/com/nbbang/domain/party/repository/PartyRepositorySupportImpl.java
+++ src/main/java/nbbang/com/nbbang/domain/party/repository/PartyRepositorySupportImpl.java
@@ -1,29 +1,27 @@
 package nbbang.com.nbbang.domain.party.repository;
 
-import com.querydsl.jpa.JPQLQuery;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import nbbang.com.nbbang.domain.party.domain.Party;
 import nbbang.com.nbbang.domain.party.domain.PartyStatus;
-import nbbang.com.nbbang.domain.party.domain.QParty;
+import static nbbang.com.nbbang.domain.party.domain.QParty.party; // JPA 책 432p.
 
 import java.util.List;
 
 @RequiredArgsConstructor
 public class PartyRepositorySupportImpl implements PartyRepositorySupport{
     private final JPAQueryFactory query;
-    QParty p = new QParty("p");
+    // QParty p = new QParty("p");
 
     @Override
     public List<Party> findByPlaceAndNotSelf(Long partyId) {
-        Party party = query.selectFrom(p).where(p.id.eq(partyId)).fetchOne();
-        List<Party> parties = query.select(p)
-                .from(p)
-                .where(p.place.eq(party.getPlace()))
-                .where(p.status.eq(PartyStatus.OPEN))
-                .where(p.id.ne(party.getId()))
-                .orderBy(p.createTime.desc())
+        Party findParty = query.selectFrom(party).where(party.id.eq(partyId)).fetchOne();
+        List<Party> parties = query.select(party)
+                .from(party)
+                .where(party.place.eq(findParty.getPlace()))
+                .where(party.status.eq(PartyStatus.OPEN))
+                .where(party.id.ne(findParty.getId()))
+                .orderBy(party.createTime.desc())
                 .limit(6)
                 .fetch();
         return parties;
