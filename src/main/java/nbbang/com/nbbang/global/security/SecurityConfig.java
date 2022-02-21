7c8b461c2f3d218faa61777229d589e31d6bc446
kenokim <keno1413@gmail.com>
2022-02-21 11:53:33 +0900

    Feat : 로그인 관련 기능 추가
    로그인 관련 기능을 추가하였습니다.

--- src/main/java/nbbang/com/nbbang/global/security/SecurityConfig.java
+++ src/main/java/nbbang/com/nbbang/global/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package nbbang.com.nbbang.global.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -33,9 +37,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/parties").permitAll()
                 .antMatchers("/login/oauth2/code/google").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
+
+                .and()
+                .httpBasic().disable().cors()
                 .and()
                 .logout().logoutUrl("/members/logout");
 
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
