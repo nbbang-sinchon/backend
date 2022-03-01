3983f638e18f2fbb13aafc4ae40ce6eb374082b6
kenokim <keno1413@gmail.com>
2022-03-01 21:13:38 +0900

    Feat : 로그인 기능 구현
    jwt 를 이용한 로그인 기능을 구현하였습니다.

--- src/main/java/nbbang/com/nbbang/global/security/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/nbbang/com/nbbang/global/security/OAuth2AuthenticationSuccessHandler.java
@@ -4,6 +4,8 @@ import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -12,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Component
@@ -20,9 +23,13 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     private String redirect_url = "http://127.0.0.1:8080";
     private String targetUri;
     private TokenProvider tokenProvider = new TokenProvider();
+    @Autowired OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+
+
         targetUri = determineTargetUri(request, response, authentication);
         getRedirectStrategy().sendRedirect(request, response, targetUri);
     }
@@ -31,9 +38,12 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         String token = tokenProvider.createToken(authentication);
         //CookieUtils.addCookie(response, "access_token", token, 360000);
         //CookieUtils.addCookie(response, "member_id", "2", 360000);
+        HttpSession session = request.getSession(false);
+        SessionMember member = (SessionMember) session.getAttribute("member");
+
         return UriComponentsBuilder.fromUriString(redirect_url)
                 .queryParam("token", token)
-                .queryParam("id", 2)
+                .queryParam("id", member.getId())
                 .build().toUriString();
     }
 
