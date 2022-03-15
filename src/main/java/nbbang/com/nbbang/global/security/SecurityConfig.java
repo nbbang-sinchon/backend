2a8a52656749779c89046926b01f4daa4f620c6d
kenokim <keno1413@gmail.com>
2022-03-15 14:50:31 +0900

    Fix : 스웨거 인증 오류 수정
    스웨거가 인증을 요구하는 오류를 수정하였습니다.

--- src/main/java/nbbang/com/nbbang/global/security/SecurityConfig.java
+++ src/main/java/nbbang/com/nbbang/global/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
@@ -42,6 +43,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new JwtAuthenticationFilter(logoutService);
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v3/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
