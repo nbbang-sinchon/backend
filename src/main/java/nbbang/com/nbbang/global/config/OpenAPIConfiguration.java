231baf5e85df6601bc6db8829890b9c124319ba2
kenokim <keno1413@gmail.com>
2022-03-28 14:03:26 +0900

    Refactor : 메시지 조회 리펙토링
    메시지 조회 관련 코드를 리펙토링하였습니다.

--- src/main/java/nbbang/com/nbbang/global/config/OpenAPIConfiguration.java
+++ src/main/java/nbbang/com/nbbang/global/config/OpenAPIConfiguration.java
@@ -5,11 +5,19 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenAPIConfiguration {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .addServersItem(new Server().url("/"));
+    }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://www.nbbang.shop/api");
+        return new OpenAPI().servers(List.of(server));
     }
 }
