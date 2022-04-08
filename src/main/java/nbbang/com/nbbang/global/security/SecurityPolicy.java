c6c0246d91701e01df9fc8ade08d980b7ab60b97
kenokim <keno1413@gmail.com>
2022-04-08 22:30:30 +0900

    Refactor : security 설정 리펙토링
    security 관련 설정을 application.properties 로 옮겼습니다.

--- src/main/java/nbbang/com/nbbang/global/security/SecurityPolicy.java
+++ src/main/java/nbbang/com/nbbang/global/security/SecurityPolicy.java
@@ -1,17 +1,14 @@
 package nbbang.com.nbbang.global.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SecurityPolicy {
-    public static int TOKEN_EXPIRE_TIME = 3600000;
-    public static String TOKEN_COOKIE_KEY = "access_token";
-    public static String DEFAULT_REDIRECT_URI = "http://127.0.0.1:3000";
-    //public static String FRONTEND_DOMAIN = "127.0.0.1";
-    public static String TOKEN_SECRET_KEY = "eyJhbGciOiJIUzUxMiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY0NjEwMjQ2MywiaWF0IjoxNjQ2MTAyNDYzfQ._l4MpJZlb32_h-aSTyF77P8Kn-PL25Hio89YK_ntpMba_z9YfDi-aJdGDSYNnOYLC9okXyxB_YvYkmZLjqhrVQ";
-
-
-
-    /*
-    Local settings
-     */
-    //public static String FRONTEND_DOMAIN = "localhost";
-    //public static String DEFAULT_REDIRECT_URI = "http://localhost:3000";
+    @Value("${jwt.expire.time:3600000}")
+    public static int TOKEN_EXPIRE_TIME;
+    @Value("${jwt.cookie.key:access_token}")
+    public static String TOKEN_COOKIE_KEY;
+    @Value("${login.default.redirection.uri:http://127.0.0.1:3000}")
+    public static String DEFAULT_REDIRECT_URI;
+    @Value("${jwt.secret.key}") // .gitignore
+    public static String TOKEN_SECRET_KEY;
 }
