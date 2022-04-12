0de969965075fe6a3c3192d5f11f8a6c92833e18
kenokim <keno1413@gmail.com>
2022-04-12 09:51:43 +0900

    Refactor : 페이징 dto abstract 적용
    PageableDto 를 abstract class 로 리펙토링 하였습니다.

--- src/main/java/nbbang/com/nbbang/global/dto/CursorPageableDto.java
+++ src/main/java/nbbang/com/nbbang/global/dto/CursorPageableDto.java
@@ -1,21 +1,9 @@
 package nbbang.com.nbbang.global.dto;
 
 import lombok.Data;
-import org.springframework.data.domain.PageRequest;
 
 @Data
-public class CursorPageableDto {
-    private Integer pageNumber;
-    private Integer pageSize;
+public abstract class CursorPageableDto extends PageableDto {
     private Long cursorId;
 
-    public PageRequest createPageRequest() {
-        if (pageNumber == null) {
-            pageNumber = 0;
-        }
-        if (pageSize == null) {
-            pageSize = 10;
-        }
-        return PageRequest.of(pageNumber, pageSize);
-    }
 }
