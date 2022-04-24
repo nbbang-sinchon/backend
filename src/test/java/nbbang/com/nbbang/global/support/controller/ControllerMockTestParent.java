12d18f403bed2bb57a9abc505941af3f00cb4abf
kenokim <keno1413@gmail.com>
2022-04-24 18:57:56 +0900

    Fix : 컨트롤러 테스트 주입 수정
    Security policy 클래스가 주입되지 않는 버그를 수정하였습니다.

--- src/test/java/nbbang/com/nbbang/global/support/controller/ControllerMockTestParent.java
+++ src/test/java/nbbang/com/nbbang/global/support/controller/ControllerMockTestParent.java
@@ -1,10 +1,7 @@
 package nbbang.com.nbbang.global.support.controller;
 
 import nbbang.com.nbbang.global.config.WebMvcConfig;
-import nbbang.com.nbbang.global.security.CustomOAuth2MemberService;
-import nbbang.com.nbbang.global.security.JwtAuthenticationManager;
-import nbbang.com.nbbang.global.security.LogoutService;
-import nbbang.com.nbbang.global.security.TokenProvider;
+import nbbang.com.nbbang.global.security.*;
 import nbbang.com.nbbang.global.security.context.CurrentMember;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.MockBeans;
@@ -21,7 +18,8 @@ import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
         @MockBean(WebMvcConfig.class),
         @MockBean(CustomOAuth2MemberService.class), @MockBean(CurrentMember.class),
         @MockBean(LogoutService.class),
-        @MockBean(JwtAuthenticationManager.class), @MockBean(TokenProvider.class)
+        @MockBean(JwtAuthenticationManager.class), @MockBean(TokenProvider.class),
+        @MockBean(SecurityPolicy.class)
 })
 public abstract class ControllerMockTestParent {
 }
